6361ca00229e783752b338fe3297756d500fcc46
david <d.marsalone@gmail.com>
2019-09-16 17:19:29 +0200

    utils humanFileSize

--- utils/FileSystemUtils.js
+++ utils/FileSystemUtils.js
@@ -97,5 +97,27 @@ class FileSystemUtils {
         let buff= Buffer.from(bitmap).toString('base64');
         return "data:image/png;base64,"+buff;
     }
+
+    /**
+     * Convertit des bytes en un truc lisible par les humains
+     * @param bytes
+     * @param si
+     * @returns {string}
+     */
+    static humanFileSize(bytes, si=true) {
+        var thresh = si ? 1000 : 1024;
+        if(Math.abs(bytes) < thresh) {
+            return bytes + ' B';
+        }
+        var units = si
+            ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
+            : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+        var u = -1;
+        do {
+            bytes /= thresh;
+            ++u;
+        } while(Math.abs(bytes) >= thresh && u < units.length - 1);
+        return bytes.toFixed(1)+' '+units[u];
+    }
 }
 module.exports = FileSystemUtils;
