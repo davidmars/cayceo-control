7868370cc0f0274819cba1968c599f77503e2608
david <d.marsalone@gmail.com>
2019-10-19 02:14:40 +0200

    0.1.50 Maj electron updater ok & clean

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const {app, BrowserWindow} = require('electron');
 const electron = require('electron');
 const path = require('path')
 const { autoUpdater } = require("electron-updater");
-
+const {ipcMain} = require('electron');
 
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -51,7 +51,7 @@ function createWindow () {
 
 
 }
-const {ipcMain} = require('electron');
+
 // Fonction reçue de CMD.INSTALL_AND_REBOOT
 ipcMain.on('INSTALL_AND_REBOOT', (event, arg) => {
   autoUpdater.quitAndInstall();
@@ -60,21 +60,20 @@ ipcMain.on('INSTALL_AND_REBOOT', (event, arg) => {
 function sendStatusToWindow(text,channel="MAJ") {
   mainWindow.webContents.send(channel, text);
 }
-
 autoUpdater.on('checking-for-update', () => {
-  sendStatusToWindow('Mise à jour ???');
+  sendStatusToWindow('Recherche de nouvelle version');
 });
 autoUpdater.on('update-available', (info) => {
-  sendStatusToWindow('Va télécharger une mise à jour');
-  //bloque les mises à jour
-  updateFound=true;
-  //les débloquera dans 10 minutes
-  updateTimeoutUnblock=setTimeout(function(){
-    updateFound=false;
-  },updateIntervalInstallingMinutes*60*1000)
+  sendStatusToWindow(`Téléchargement de v.${info.version }`);
 });
 autoUpdater.on('update-not-available', (info) => {
-  sendStatusToWindow('');
+  sendStatusToWindow("Le programme est à jour");
+  setTimeout(function(){
+    sendStatusToWindow("");
+  },3*1000);
+  setTimeout(function(){
+    autoUpdater.checkForUpdatesAndNotify();
+  },60*1000);
 });
 autoUpdater.on('error', (err) => {
   sendStatusToWindow('Error ' + err);
@@ -83,67 +82,27 @@ autoUpdater.on('download-progress', (progressObj) => {
   //let log_message = "Download speed: " + progressObj.bytesPerSecond;
   //log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
   //log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-  sendStatusToWindow(`Mise à jour ${Math.round(progressObj.percent)}%`);
+  //sendStatusToWindow(`Mise à jour ${Math.round(Number(progressObj.percent))}%`);
+  sendStatusToWindow("download-progress");
+  sendStatusToWindow(progressObj.percent);
 });
 autoUpdater.on('update-downloaded', (info) => {
-  sendStatusToWindow('Installer la mise à jour',"MAJ done");
-  updateLoop(true)
+  sendStatusToWindow(`Installer v.${info.version }`,"MAJ done");
 });
-
-//temps entre deux test de nouvelle version
-let updateIntervalMinutes=1;
-//temps donné pour une installation
-let updateIntervalInstallingMinutes=30;
-let updateTimeout=null;
-let updateTimeoutUnblock=null;
-let updateFound=false;
-let updateLoop=function(stop=false){
-  if(updateTimeout) {
-    clearTimeout(updateTimeout);
-  }
-  if(stop){
-    if(updateTimeoutUnblock) {
-      clearTimeout(updateTimeoutUnblock);
-    }
-    return;
-  }
-  if(!updateFound){
-    sendStatusToWindow('Mise à jour ?');
-    autoUpdater.checkForUpdatesAndNotify();
-  }else{
-    sendStatusToWindow('Va télécharger une mise à jour...');
-  }
-  updateTimeout=setTimeout(function(){
-    updateLoop();
-  },updateIntervalMinutes*60*1000);
-};
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+});
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+});
 app.on('ready', function(){
   createWindow();
-  setInterval(function(){
-    updateLoop();
+  setTimeout(function(){
+    autoUpdater.checkForUpdatesAndNotify();
   },5*1000);
 
 });
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
-})
-
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
 
 
 
