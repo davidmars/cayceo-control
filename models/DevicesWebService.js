fdc00f8ccabcbb406f5e2a14fc9f3a0d6351497b
david <d.marsalone@gmail.com>
2020-06-18 14:11:57 +0200

    0.6.6 délai d'envoi de machine status adaptatif si ipneo remote n'est pas activé

--- models/DevicesWebService.js
+++ models/DevicesWebService.js
@@ -17,60 +17,73 @@ class DevicesWebService {
          * @type {Machine}
          */
         this.machine = machine;
-        setInterval(function(){
+        this._sendInfo();
+    }
+
+
+    _sendInfo(){
+        let me=this;
+
+        //appel récursif avec gestion de l'intervale selon si app mobile activée ou non
+        if(sync.getJukebox().ipneoremote){
+            this.interval=5;
+        }else{
+            this.interval=60;
+        }
+        setTimeout(function(){
             me._sendInfo();
         },me.interval*1000);
-    }
-    _sendInfo(){
 
         let devices=[];
+        if(typeof casquesManager !== "undefined"){
+            for(let casque of casquesManager.casquesList()){
+                let c={};
+                c.id=ipToNumero(casque.ip);
+                c.isPlugged=casque.plugged;
+                c.batteryLevel=Number(casque.batteryLevel);
+                c.ip=casque.ip;
+                c.apk=casque.apkInfos.lastApk;
 
-        for(let casque of casquesManager.casquesList()){
-            let c={};
-            c.id=ipToNumero(casque.ip);
-            c.isPlugged=casque.plugged;
-            c.batteryLevel=Number(casque.batteryLevel);
-            c.ip=casque.ip;
-            c.apk=casque.apkInfos.lastApk;
+                c.state="offline";
+                if(casque.online) {
+                    let casqueUi=ui.casques.getCasqueByIp(casque.ip);
 
-            c.state="offline";
-            if(casque.online) {
-                let casqueUi=ui.casques.getCasqueByIp(casque.ip);
-
-                if(casqueUi){
-                    if(casqueUi.isSelectable()){
-                        c.state = "ready";
-                    }
-                    if(casqueUi.contenu){
-                        c.session={};
-                        c.session.playTime=Number(casque.nowPlaying.remainingSeconds)*-1;
-                        let contenu=ui.films.getFilmByFilePath(casque.nowPlaying.contenuPath);
-                        if(contenu){
-                            c.session.pictureUrl=contenu.imgHttp;
-                            c.session.name=contenu.title;
+                    if(casqueUi){
+                        if(casqueUi.isSelectable()){
+                            c.state = "ready";
                         }
-                        if(casque.nowPlaying.isPlaying){
-                            c.state="in_progress";
-                        }else{
-                            c.state="paused";
+                        if(casqueUi.contenu){
+                            c.session={};
+                            c.session.playTime=Number(casque.nowPlaying.remainingSeconds)*-1;
+                            let contenu=ui.films.getFilmByFilePath(casque.nowPlaying.contenuPath);
+                            if(contenu){
+                                c.session.pictureUrl=contenu.imgHttp;
+                                c.session.name=contenu.title;
+                            }
+                            if(casque.nowPlaying.isPlaying){
+                                c.state="in_progress";
+                            }else{
+                                c.state="paused";
+                            }
                         }
                     }
                 }
-            }
 
-            devices.push(c);
-        }
-        $.ajax(this.syncUrl,{
-            data:{
-                uid:sync.getJukebox().uid,
-                devices:devices
-            },
-            success:function(json){
-                console.log("DevicesWebService",json)
-            },error:function(err){
-                console.error("DevicesWebService",err)
+                devices.push(c);
             }
-        })
+            $.ajax(this.syncUrl,{
+                data:{
+                    uid:sync.getJukebox().uid,
+                    devices:devices
+                },
+                success:function(json){
+                    console.log("DevicesWebService",json)
+                },error:function(err){
+                    console.error("DevicesWebService",err)
+                }
+            })
+        }
+
     }
 }
 
